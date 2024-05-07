04bdf004e24bf523afcff7b44c9de9cb186124af
gabicostag27 <gabrielacostag27@gmail.com>
2024-05-07 10:25:02 -0300

    registrando a blueprint

--- app.py
+++ app.py
@@ -2,33 +2,37 @@ from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
+
+from api import bp
+app.register_blueprint(bp)
+
 @app.route("/", methods=("GET",))
 def index():
     return jsonify({"status": 200, "message": "API da Gabriela da Costa"})
 
 
-@app.route("/api/aleatorios", methods=("GET",))
+@app.route("/aleatorios", methods=("GET",))
 def aleatorios():
     import random
     a = random.randint(49, 100)
     return jsonify({"status": 200, "data": a})
 
-@app.route("/api/argumentos/<string:nome>", methods=("GET",))
+@app.route("/argumentos/<string:nome>", methods=("GET",))
 def argumentos(nome: str):
     return jsonify({"status": 200, "data": nome})
 
-@app.route("/api/argumentos", methods=("GET",))
+@app.route("/argumentos", methods=("GET",))
 def arg_implicito():
     return jsonify({"status": 200, "data": request.args["nome"]})
 
-@app.route("/api/idades", methods=("GET",))
+@app.route("/idades", methods=("GET",))
 def idades():
     from random_data import pessoas
     import funcoes
     num = funcoes.maior_de_50(pessoas)
     return jsonify({"status": 200, "data": num})
 
-@app.route("/api/salario", methods=("GET",))
+@app.route("/salario", methods=("GET",))
 def salario():
     from random_data import pessoas
     import funcoes
